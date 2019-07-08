cc150ed12123a487266328ab95b479612eb83b81
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2019-07-08 12:15:17 +0530

    first app

---
+++ hello_flutter/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Test App"),
+        ),
+        body: HelloWorld(),
+      ),
+    ),
+  );
+}
+
+class HelloWorld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.blueAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
